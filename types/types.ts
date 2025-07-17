b3c764f28ea3f2242fdbf5cab5822489efdcf728
wilsonaustin10 <107770258+wilsonaustin10@users.noreply.github.com>
2025-07-17 15:44:28 -0500

    Add src/types/types.ts from template

---
+++ types/types.ts
@@ -0,0 +1,14 @@
+export interface LeadFormData {
+    address: string;
+    phone: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    timeframe?: string;
+    propertyCondition?: string;
+    reasonForSelling?: string;
+    askingPrice?: string;
+    bedrooms?: string;
+    bathrooms?: string;
+    squareFeet?: string;
+  }
