b2fd62cd505191932e7809b965c9a0afb0c8596a
wilsonaustin10 <107770258+wilsonaustin10@users.noreply.github.com>
2025-07-17 15:44:12 -0500

    Add src/app/layout.tsx from template

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { FormProvider } from '../context/FormContext';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Script from 'next/script';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+          strategy="beforeInteractive"
+          onLoad={() => {
+            console.log('Google Maps script loaded');
+          }}
+          onError={(e) => {
+            console.error('Error loading Google Maps script:', e);
+          }}
+        />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17041108639"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17041108639');
+            `,
+          }}
+        />
+      </head>
+      <body className={inter.className}>
+        <FormProvider>
+          <Header />
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer />
+        </FormProvider>
+      </body>
+    </html>
+  );
+} 
